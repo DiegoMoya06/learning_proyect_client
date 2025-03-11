005fc8162edc9eba39053df40aaeb817935de884
Diego Moya <diego0697ms@gmail.com>
2025-03-11 12:08:56 +0100

    Added env variables file and removed TODOs

--- src/services/deepSeekApiService.ts
+++ src/services/deepSeekApiService.ts
@@ -3,11 +3,10 @@ import axios from 'axios';
 
 const DEEPSEEK_API_URL = '/api';
 // Change to ENV_VARIABLE
-const API_KEY = 'api key'; // Replace with your actual API key
+const API_KEY = 'api key';
 
 export const callDeepSeekAPI = async (inputText: string, file: File) => {
     try {
-        // TODO: check if content might be adapted/deleted to call springboot instead
         // Create a FormData object
         const formData = new FormData();
         formData.append('input', inputText); // Append the input text
